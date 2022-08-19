eff7d24b5778b959cbbecee4564838a026f2c740
caio-felipe <106115913+Caiiobill@users.noreply.github.com>
2022-08-18 22:12:20 -0300

    Add files via upload

---
+++ pizzas.js
@@ -0,0 +1,162 @@
+let pizzaJson = [
+  {
+    id: 1,
+    name: '1. MISTO QUENTE',
+    img: 'lanche/1-misto-quente.png',
+    price: [7.0, 7.0, 7.0],
+    sizes: [''],
+    description: 'Mussarela, Presunto, Tomate, milho verde, ketchup e maionese'
+  },
+  {
+    id: 2,
+    name: '2. HAMBURGUER',
+    img: 'lanche/2-hamburguer.png',
+    price: [7.0, 7.0, 7.0],
+    sizes: [''],
+    description: 'Hamburguer, Tomate, milho verde, Catchup, Maionese. e Cebola'
+  },
+  {
+    id: 3,
+    name: '3. AMERICANO',
+    img: 'lanche/6 X-EGGSBURGUERS.png',
+    price: [9.0, 9.0, 9.0],
+    sizes: [''],
+    description:
+      'Ovo, Presunto, Mussarela, Tomate, milho verde, Catchup, Maionese.'
+  },
+  {
+    id: 4,
+    name: '4. EGGSBURGUERS',
+    img: 'lanche/2-hamburguer.png',
+    price: [9.0, 9.0, 9.0],
+    sizes: [''],
+    description:
+      'Hamburguer, Ovo, Presunto, Mussarela, Tomate, milho verde, Catchup, Maionese'
+  },
+  {
+    id: 5,
+    name: '5. X-BURGUERS',
+    img: 'lanche/5 X-BURGUERS.png',
+    price: [11.0, 11.0, 11.0],
+    sizes: [''],
+    description:
+      'Hamburguer, Presunto, Mussarela, Tomate, milho verde, Catchup. Maionese.'
+  },
+  {
+    id: 6,
+    name: '6. X-EGGSBURGUERS',
+    img: 'lanche/6 X-EGGSBURGUERS.png',
+    price: [11.0, 11.0, 11.0],
+    sizes: [''],
+    description:
+      'Hamburguer, Ovo, Presunto, Mussarela, Tomate, milho verde, Catchup, Maionese.'
+  },
+  {
+    id: 7,
+    name: '7. X-FRANGO',
+    img: 'lanche/7 x-frangoo.png',
+    price: [13.0, 13.0, 13.0],
+    sizes: [''],
+    description:
+      'Frango, Mussarela, Presunto, Tomate, Catchup, Maionese e milho verde.'
+  },
+  {
+    id: 8,
+    name: '8. BACON',
+    img: 'lanche/8-x-baconn.png',
+    price: [13.0, 13.0, 13.0],
+    sizes: [''],
+    description:
+      'Bacon, Presunto, Mussarela, Tomate, milho verde, Catchup, Maionese.'
+  },
+  {
+    id: 9,
+    name: '9. X-CALABRESA',
+    img: 'lanche/9 x-calabresaa.png',
+    price: [13.0, 13.0, 13.0],
+    sizes: [''],
+    description:
+      'Calabresa, Presunto, Mussarela, Tomate, milho verde, Catchup, Maionese.'
+  },
+  {
+    id: 10,
+    name: '10. X-BACON',
+    img: 'lanche/8-x-baconn.png',
+    price: [15.0, 15.0, 15.0],
+    sizes: [''],
+    description:
+      'Hamburguer, Bacon, Presunto, Mussarela, Tomate, milho verde, Catchup, Maionese.'
+  },
+  {
+    id: 11,
+    name: '11. FRAMBURGUER',
+    img: 'lanche/11 -FRAMBURGUERr.png',
+    price: [15.0, 15.0, 15.0],
+    sizes: [''],
+    description:
+      'Hamburguer, Frango, Presunto, Mussarela, Tomate, milho verde, Catchup, Maionese'
+  },
+  {
+    id: 12,
+    name: '12. FRAMBACON',
+    img: 'lanche/12-FRAMBACONn.png',
+    price: [15.0, 15.0, 15.0],
+    sizes: [''],
+    description:
+      'Frango, Bacon, Presunto, Mussarela, Tomate, milho verde, Catchup, Maionese.'
+  },
+  {
+    id: 13,
+    name: '13. CALAFRANGO',
+    img: 'lanche/13-CALAFRANGO.png',
+    price: [15.0, 15.0, 15.0],
+    sizes: [''],
+    description:
+      'Frango, Calabresa, Presunto, Mussarela, Tomate, milho verde, Catchup, Maionese.'
+  },
+  {
+    id: 14,
+    name: '14. CALABRESA',
+    img: 'lanche/14-CALABRe.jpg',
+    price: [15.0, 15.0, 15.0],
+    sizes: [''],
+    description:
+      'Hamburguer, Calabresa, Presunto, Mussarela, Tomate, milho verde, Catchup, Maionese.'
+  },
+  {
+    id: 15,
+    name: '15. CATUPIRY',
+    img: 'lanche/2-hamburguer.png',
+    price: [15.0, 15.0, 15.0],
+    sizes: [''],
+    description:
+      'Catupiry, Frango, Presunto, Mussarela, Tomate, milho verde, Catchup, Maionese.'
+  },
+  {
+    id: 16,
+    name: '16. CAIPIRA',
+    img: 'lanche/16-caipiraa.png',
+    price: [16.0, 16.0, 16.0],
+    sizes: [''],
+    description:
+      'Catupiry, Hamburguer, Bacon, Presunto, Mussarela, Tomate, milho verde, Catchup, Maionese.'
+  },
+  {
+    id: 17,
+    name: '17. A MODA DA CASA',
+    img: 'lanche/17-amodadacasa.png',
+    price: [216.0, 16.0, 16.0],
+    sizes: [''],
+    description:
+      'Catupiry, Hamburguer, Calabresa, Presunto, Mussarela, Tomate, milho verde, Catchup, Maionese.'
+  },
+  {
+    id: 18,
+    name: '18. BIG BAGUNÇA',
+    img: 'lanche/18-bigg.png',
+    price: [20.0, 20.0, 20.0],
+    sizes: [''],
+    description:
+      'Hamburguer, Bacon, Ovo, Frango Presunto, Mussarela, Tomate, milho verde, Catchup, Maionese'
+  }
+]
